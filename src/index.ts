9d091b24b3800090ca938d1a060bfa84c1349cbe
qinglong.su <suqinglong01@gmail.com>
2020-04-16 10:20:38 +0800

    node search

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,25 @@
 import puppeteer from 'puppeteer'
 import { PowerDataComSite } from "./sites/power.dat.com";
 import memory from "./tools/memory";
-// import cheerio from 'cheerio'
 
-// const $ = cheerio.load('<div><span><a class="test">aa</a></span><span><a>bb</a></span></div>')
+puppeteer.launch({
+  // executablePath:'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  // devtools: false,
+  // headless: false,
+  // defaultViewport: {
+  //   width: 1200,
+  //   height: 800
+  // }
 
-// const html = Array.from($('span')).map((item: any) => {
-//   return $(item).find('a.test').html();
-// })
-
-// console.log(html)
-
-// puppeteer.launch({
-//   // executablePath:'/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-//   // devtools: false,
-//   // headless: false,
-//   // defaultViewport: {
-//   //   width: 1200,
-//   //   height: 800
-//   // }
-
-//   args: [
-//     'no-sandbox',
-//     'disable-setuid-sandbox',
-//   ]
-// }).then(async (browser: any) => {
-//   const site = new PowerDataComSite(browser)
-//   await site.prePare();
-//   await site.doTask();
-// });
+  args: [
+    'no-sandbox',
+    'disable-setuid-sandbox',
+  ]
+}).then(async (browser: any) => {
+  const site = new PowerDataComSite(browser)
+  await site.prePare();
+  await site.doTask();
+});
 
 // setInterval(() => {
 //   console.log(memory.memory());

9b266a64c6961d7377a5eebcb2ae13efbf02deac
qinglong.su <suqinglong01@gmail.com>
2020-04-22 17:57:35 +0800

    add new site

--- src/sites/echodrive.echo.com.ts
+++ src/sites/echodrive.echo.com.ts
@@ -1,30 +1,98 @@
 import { SearchSite } from './search.site';
 import { account } from '../account';
 import puppeteer from 'puppeteer';
+import dateformat from 'dateformat';
+import cheerio from 'cheerio';
+import { Trim, ModifyPostData } from '../tools/index';
+import { Post } from '../api';
 
 export class EchodriveEchoCom extends SearchSite {
-    private url = 'https://echodrive.echo.com/v2/login';
-    private searchPage = 'https://echodrive.echo.com/v2/carrier/3275/availableLoads';
-    private name: string = account['echodrive.echo.com'].name;
-    private password: string = account['echodrive.echo.com'].password;
-    private page: puppeteer.Page;
+  private url = 'https://echodrive.echo.com/v2/login';
+  private searchPage = 'https://echodrive.echo.com/v2/carrier/3275/availableLoads';
+  private name: string = account['echodrive.echo.com'].name;
+  private password: string = account['echodrive.echo.com'].password;
+  private page: puppeteer.Page;
 
-    public async prePare() {
-        try {
-            console.log('EchodriveEchoCom  begin prePare');
-            this.page = await this.browser.newPage();
-            await this.page.goto(this.url);
-            await this.page.type('#email-input', this.name);
-            await this.page.type('#password-input', this.password);
-            this.page.click('#loading-button-component');
-            await this.page.waitForNavigation();
-            await this.page.goto(this.searchPage);
-        } catch (e) {
-            console.log('EchodriveEchoCom  prepare error', e);
+  public async prePare() {
+    try {
+      console.log('EchodriveEchoCom  begin prePare');
+      this.page = await this.browser.newPage();
+      await this.page.goto(this.url);
+      await this.page.type('#email-input', this.name);
+      await this.page.type('#password-input', this.password);
+      this.page.click('#loading-button-component');
+      await this.page.waitForNavigation();
+      await this.page.goto(this.searchPage);
+      await this.page.waitForSelector('.search-btn', {
+        timeout: 5000
+      });
+    } catch (e) {
+      console.log('EchodriveEchoCom  prepare error', e);
+    }
+  }
+
+  public async search(task: ITASK) {
+    console.log('EchodriveEchoCom  EchodriveEchoCom search')
+    await this.page.type('.origin-input input', task.criteria.origin)
+    await this.page.type('.dho-input input', task.criteria.origin_radius)
+    await this.page.type('.destination-input input', task.criteria.destination)
+    await this.page.type('.dhd-input input', task.criteria.destination_radius)
+    // Apr 22 - 25
+    await this.page.type('.date-input input', dateformat(task.criteria.pick_up_date, 'mmm-dd'))
+    await this.page.click('.search-btn')
+    await this.page.waitForSelector('.available-loads-row')
+    const resultHtml = await this.page.$eval(".loads-bids-container", (res) => res.innerHTML)
+    const $ = cheerio.load(resultHtml)
+    Post(this.getDataFromHtml($, task.task_id)).then((res: any) => {
+      console.log('EchodriveEchoCom', res.data);
+    });
+  }
+
+  private getDataFromHtml($: CheerioStatic, taskID: string): Array<IResultData> {
+    const result: any = {};
+
+    const dataItemClass = [
+      '.origin',
+      ['.origin-dh', 'origin_radius'],
+      '.destination',
+      ['.destination-dh', 'destination_radius'],
+      '.loaded-miles',
+      '.weight',
+      '.equipment'
+    ]
+
+    const resultItemRows = $('available-loads-results-row .app-card').map((key, element) => {
+      console.info(key)
+      const resultItem: any = {};
+      dataItemClass.forEach((item) => {
+        let key: string;
+        let selector: string;
+        if (Array.isArray(item)) {
+          [selector, key] = item;
+        } else {
+          key = item.substr(1);
+          selector = item;
         }
+        resultItem[key] = Trim($(element).find(selector).text());
+      });
+      return resultItem
+    });
+
+    const resultItemDetails = $('available-loads-results-detail .column.info').map((key, element) => {
+      const resultItem: any = {};
+      console.info(key)
+      $(element).find('.row').each((key, row) => {
+        const $row = $(row)
+        resultItem[Trim($row.find('.title').text())] = Trim($row.find('.details').text())
+      })
+      return resultItem
+    })
+
+    const records = []
+    for (let i = 0, len = resultItemRows.length; i < len; i++) {
+      records.push({ ...resultItemRows[i], ...resultItemDetails[i] })
     }
 
-    public async search(task: ITASK) {
-      console.log('EchodriveEchoCom  EchodriveEchoCom search')
-    }
+    return ModifyPostData(taskID, records)
+  }
 }
--- src/test.js
+++ src/test.js
@@ -1,75 +1,77 @@
-const cheerio = require('cheerio');
-const fs = require('fs');
+// const cheerio = require('cheerio');
+// const fs = require('fs');
+const dateformat = require('dateformat')
+console.log(dateformat(new Date(), 'mmm-dd'))
 
-fs.readFile('./src/test.html', 'utf8', function (err, data) {
-    if (err) throw err;
-    const $ = cheerio.load(data);
+// fs.readFile('./src/test.html', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     const $ = cheerio.load(data);
 
-    // td data
+//     // td data
 
-    const dataItemClass = [
-        '.age',
-        '.avail',
-        ['.truck', 'equipment'],
-        '.fp',
-        ['.do', 'origin_radius'],
-        '.origin',
-        '.trip',
-        ['.dest', 'destination'],
-        ['.dd', 'destination_radius'],
-        '.company',
-        '.contact',
-        '.length',
-        '.weight'
-    ];
+//     const dataItemClass = [
+//         '.age',
+//         '.avail',
+//         ['.truck', 'equipment'],
+//         '.fp',
+//         ['.do', 'origin_radius'],
+//         '.origin',
+//         '.trip',
+//         ['.dest', 'destination'],
+//         ['.dd', 'destination_radius'],
+//         '.company',
+//         '.contact',
+//         '.length',
+//         '.weight'
+//     ];
 
-    dataItemClass.forEach((item) => {
-        let key;
-        let selector;
-        if (Array.isArray(item)) {
-            [selector, key] = item;
-        } else {
-            key = item.substr(1);
-            selector = item;
-        }
-        console.log(
-            key,
-            $(selector)
-                .text()
-                .replace(/\n/g, '')
-                .replace(/\s{2,}/g, ' ')
-        );
-    });
+//     dataItemClass.forEach((item) => {
+//         let key;
+//         let selector;
+//         if (Array.isArray(item)) {
+//             [selector, key] = item;
+//         } else {
+//             key = item.substr(1);
+//             selector = item;
+//         }
+//         console.log(
+//             key,
+//             $(selector)
+//                 .text()
+//                 .replace(/\n/g, '')
+//                 .replace(/\s{2,}/g, ' ')
+//         );
+//     });
 
-    // detail data
-    const arr = $('dl > dt, dl > dt + dd');
-    const key_value_arr = [];
-    arr.each((key, item) => {
-        key_value_arr.push(
-            $(item)
-                .text()
-                .trim()
-                .replace(/\n/g, '')
-                .replace(/\s{2,}/g, ' ')
-        );
-    });
+//     // detail data
+//     const arr = $('dl > dt, dl > dt + dd');
+//     const key_value_arr = [];
+//     arr.each((key, item) => {
+//         key_value_arr.push(
+//             $(item)
+//                 .text()
+//                 .trim()
+//                 .replace(/\n/g, '')
+//                 .replace(/\s{2,}/g, ' ')
+//         );
+//     });
 
-    const result = {};
-    for (let i = 0; i < key_value_arr.length; i += 2) {
-        result[key_value_arr[i]] = key_value_arr[i + 1];
-    }
+//     const result = {};
+//     for (let i = 0; i < key_value_arr.length; i += 2) {
+//         result[key_value_arr[i]] = key_value_arr[i + 1];
+//     }
 
-    const borkerToCarrierSpot = [
-        '.widget-title-incl-text',
-        '.widget-numbers-num',
-        '.widget-numbers-range'
-    ];
+//     const borkerToCarrierSpot = [
+//         '.widget-title-incl-text',
+//         '.widget-numbers-num',
+//         '.widget-numbers-range'
+//     ];
 
-    const borkerToCarrierSpotData = {
-        'Broker-to-Carrier Spot': borkerToCarrierSpot.map((item) => {
-            return $(item).text();
-        })
-    };
+//     const borkerToCarrierSpotData = {
+//         'Broker-to-Carrier Spot': borkerToCarrierSpot.map((item) => {
+//             return $(item).text();
+//         })
+//     };
 
-    console.log(result, borkerToCarrierSpotData);
-});
+//     console.log(result, borkerToCarrierSpotData);
+// });

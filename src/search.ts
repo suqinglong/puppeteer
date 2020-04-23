3f15278ab54685a24a492b270603673b564ea8e4
qinglong.su <suqinglong01@gmail.com>
2020-04-23 16:03:59 +0800

    modify search logic

--- src/search.ts
+++ src/search.ts
@@ -4,19 +4,15 @@ import { EchodriveEchoCom } from './sites/echodrive.echo.com';
 import { Settings } from './settings';
 import { getMode } from './tools/index';
 import { SearchSite } from './sites/search.site';
-import { SingletonTedis } from './tools/tedis';
-import { Tedis } from 'tedis';
 
 const sites = [EchodriveEchoCom, PowerDatComSite];
 
 export class Search implements ISearchClass {
-    private tedis: Tedis = new SingletonTedis().getInstance();
     private mode: IMode = getMode();
     private settings = this.mode === 'develop' ? Settings : {};
     private searchSites: { [key: string]: SearchSite } = {};
 
     public async prepare() {
-        this.developPrepare();
         const browser = await puppeteer.launch({
             ...this.settings,
             args: ['no-sandbox', 'disable-setuid-sandbox']
@@ -26,6 +22,9 @@ export class Search implements ISearchClass {
 
     public async doTask(task: ITASK) {
         const matchedSite = this.searchSites[task.site];
+        if (!matchedSite) {
+            return
+        }
         if (!matchedSite.isLogin) {
             await matchedSite.prePare(task.email, task.password);
         }
@@ -40,26 +39,5 @@ export class Search implements ISearchClass {
             const site = new SiteClass(browser);
             this.searchSites[site.siteName] = site;
         });
-    }
-
-    private developPrepare() {
-        if (this.mode === 'develop') {
-            const taskResult = JSON.stringify({
-                email: 'primelinkexpress@live.com',
-                password: 'Gary1978',
-                user_id: '3',
-                task_id: 'ca7eb2b1c5c98467ae4809d95bdc5446',
-                site: 'XPO Connect',
-                criteria: {
-                    origin: 'Simsboro, LA',
-                    origin_radius: '100',
-                    destination: 'Luray, VA',
-                    destination_radius: '100',
-                    pick_up_date: '2020-04-22',
-                    equipment: 'Van'
-                }
-            });
-            this.tedis.lpush('search_tasks', taskResult);
-        }
     }
 }
--- src/tasks.ts
+++ src/tasks.ts
@@ -1,12 +1,15 @@
 import { Search } from './search';
 import { SingletonTedis } from './tools/tedis';
 import { Tedis } from 'tedis';
+import { getMode } from './tools/index';
 
 export class Tasks implements ITasksClass {
   private tedis: Tedis = new SingletonTedis().getInstance();
   private searchMap: { [key: string]: Search } = {}
+  private mode: IMode = getMode();
 
   public async getTask() {
+    this.developPrepare()
     while (true) {
       const taskResult = (await this.tedis.blpop(0, 'search_tasks'))[1];
       if (taskResult) {
@@ -20,4 +23,25 @@ export class Tasks implements ITasksClass {
       }
     }
   }
+
+  private developPrepare() {
+    if (this.mode === 'develop') {
+        const taskResult = JSON.stringify({
+            email: 'primelinkexpress@live.com',
+            password: 'Gary1978',
+            user_id: '3',
+            task_id: 'ca7eb2b1c5c98467ae4809d95bdc5446',
+            site: 'XPO Connect',
+            criteria: {
+                origin: 'Simsboro, LA',
+                origin_radius: '100',
+                destination: 'Luray, VA',
+                destination_radius: '100',
+                pick_up_date: '2020-04-22',
+                equipment: 'Van'
+            }
+        });
+        this.tedis.lpush('search_tasks', taskResult);
+    }
+}
 }

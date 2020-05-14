d55e8572b6c7418708226afb27afc83acf2d1f54
shijie.gao <shijie.gao@yang.exchange>
2020-05-14 15:14:55 +0800

    test

--- src/gaoshijie.ts
+++ src/gaoshijie.ts
@@ -2,6 +2,8 @@ import puppeteer from 'puppeteer';
 import cheerio from 'cheerio';
 import dateformat from 'dateformat';
 
+const {TimeoutError} = require('puppeteer/Errors');
+
 puppeteer
     .launch({
         headless: true,
@@ -355,10 +357,19 @@ puppeteer
 
         page.once('load', () => console.log('页面加载完成。'));
 
+        // debug 用的,可以看到点击按钮有没有成功发出请求
         page.on('requestfinished', (request) => {
+
+            if (request.url().indexOf('www.landstaronline.com') == -1) {
+                return;
+            }
+
             console.log(
                 'on request finished. url:' + request.url() + ' method: ' + request.method()
             );
+            if (request.method() == "POST") {
+                console.log(" post data: " + request.postData());
+            }
         });
 
         const url = 'http://www.landstaronline.com/public/login.aspx';
@@ -374,18 +385,215 @@ puppeteer
         await page.type('#PASSWORD', 'Loaded2020');
 
         // 点击登录
-        // 点击登录后,触发跳转,会跳转几次,最新跳转到 http://spportal.landstaronline.com/
-        //await page.waitForSelector('#Submit');
+        // 点击登录后,触发跳转,会跳转几次,最终会跳转到 http://spportal.landstaronline.com/
+        await page.waitForSelector('#Submit');
+        await page.click('#Submit');
 
-        const [response] = await Promise.all([
-            page.waitForNavigation(),
-            page.waitForSelector('#Submit'),
-        ]);
+        // console.log('sleep 5 seconds');
+        // await page.waitFor(5000);
 
-        console.log(response);
+        // 最多等 5 秒,如果登录成功会跳转到首页
+        let login = false;
+        try {
+            await page.waitForSelector('#dashboard', {timeout: 5000});
+            console.log("登录成功。");
+
+            const url_ = page.url();
+            console.log("登录成功后的url是: " + url_);
+
+            login = true;
+        } catch (e) {
+            if (e instanceof TimeoutError) {
+                console.log("登录失败。");
+                // todo 打日志、截图、调用接口上报
+            }
+        }
+
+        if (login) {
+            const searchUrl = 'http://www.landstaronline.com/loads';
+
+            await page.goto(searchUrl, { waitUntil: 'load' });
+
+            // origin radius
+            await page.waitForSelector('#OriginRadius');
+            await page.select('#OriginRadius', '150'); // 可选值:0, 25, 50, 75, 100, 150, 200, 250, 300, 350, 400, 450, 500
+
+            // destination radius
+            await page.waitForSelector('#DestinationRadius');
+            await page.select('#DestinationRadius', '150');
+
+            // 限制只搜索美国,不搜索加拿大
+            await page.waitForSelector('#OriginRestrictResults');
+            await page.select('#OriginRestrictResults', 'US');
+
+            await page.waitForSelector('#DestinationRestrictResults');
+            await page.select('#DestinationRestrictResults', 'US');
+
+            // pickup start date
+            await page.waitForSelector('#PickupDateStart');
+            await page.type('#PickupDateStart', '05/13/2020');
+
+            // pickup end date 不用输入, 输入 start date 时,会自动填充 end date
+            //await page.waitForSelector('#PickupDateEnd');
+            //await page.type('#PickupDateEnd', '05/13/2020');
+
+            // origin
+            await page.waitForSelector('#TxtOriginControl');
+            await page.type('#TxtOriginControl', 'Atwater, CA');
+
+            await page.waitFor(1000); // 等 ajax 请求成功
+
+            // 此时会出现智能提示
+            await page.waitForSelector('#ui-id-1 li', {timeout: 3000});
+
+            let temp = await page.$$('#ui-id-1 li');
+            // 如果只有一个,就 click
+            if (temp.length === 1) {
+                await temp[0].click();
+                console.log('找到一个 origin');
+            } else {
+                console.log('没有找到 origin');
+                return;
+            }
+
+            // destination
+            await page.waitForSelector('#TxtDestinationControl');
+            await page.type('#TxtDestinationControl', 'Sumner, WA');
+
+            await page.waitFor(1000); // 等 ajax 请求成功
+
+            // 此时会出现智能提示
+            await page.waitForSelector('#ui-id-2 li', {timeout: 3000});
+
+            let temp2 = await page.$$('#ui-id-2 li');
+            // 如果只有一个,就 click
+            if (temp2.length === 1) {
+                await temp2[0].click();
+                console.log('找到一个 destination');
+            } else {
+                console.log('没有找到 destination');
+                return;
+            }
+
+            // 选择 equipment
+            const search_equipment = "VAN"; // 可选的值是 VAN 和 REFR ,注意不是 REEFER
+            await page.evaluate((search_equipment) => {
+
+                const i =( document.querySelector('#ddArrowTrailerTypes') as HTMLElement);
+                i.click();
+
+                document.querySelectorAll('#treeDivmultiSelectTrailerTypes > ul>li').forEach((element) => {
+                    const name = (element.querySelector('div span.k-in') as HTMLElement).innerText;
+                    console.log(name);
+                    if (name == search_equipment) {
+                        const checkbox = element.querySelector('div span.k-checkbox input') as HTMLElement;
+                        checkbox.click();
+                    }
+                });
+
+                i.click();
+
+            }, search_equipment);
 
 
+            // 点击 search ,会跳转到 http://www.landstaronline.com/Loads/Load/SearchResults
+            // 查看  http://www.landstaronline.com/Loads/Load/PerformSearch 这个请求的 post data 是不是期望的
 
+            await page.waitForSelector('#searchButton');
+            console.log("找到搜索按钮");
+
+            // await page.click('#searchButton'); // 不 work
+            await page.evaluate(() => {
+                let btn: HTMLElement = document.querySelector('#searchButton') as HTMLElement;
+                btn.click();
+            });
+
+            // 等待跳转
+            console.log('等待跳转');
+            // await page.waitFor(1000);
+
+            let success = false;
+            try {
+                // #ResultsTabs 这个div 就是存放搜索结果的地方
+                await page.waitForSelector('#ResultsTabs', {timeout: 5000});
+                console.log('搜索成功。');
+
+                success = true;
+            } catch (e) {
+                if (e instanceof TimeoutError) {
+                    console.log("搜索失败。");
+                }
+            }
+
+            if (success) {
+
+                const resultHtml = await page.$eval('div#Loads', (e) => e.innerHTML);
+
+                console.log(resultHtml);
+
+                const $ = cheerio.load(resultHtml);
+
+                if ($('tbody tr').length == 1) {
+                    console.log("没有符合条件的数据。");
+                } else {
+
+                    $('tbody tr.t-master-row').each((_index, _item) => {
+
+                        const tds = $(_item).find('td');
+                        
+
+                        let t = $(tds[2]).text().trim().split("\n");
+                        let t1 = t[0].trim();
+                        let t2 = t[1].trim();
+                        console.log("agency: " + t1);
+                        console.log("contact: " + t2);
+
+                        t = $(tds[3]).text().trim().split("\n");
+                        t1 = t[0].trim();
+                        t2 = t[1].trim();
+                        console.log("pickup date: " + t1);
+                        console.log("delivery date: " + t2);
+
+                        t = $(tds[4]).text().trim().split("\n");
+                        t1 = t[0].trim();
+                        t2 = t[2].trim();
+                        console.log("origin: " + t1);
+                        console.log("destination: " + t2);
+
+
+                        t = $(tds[5]).text().trim().split("\n");
+                        t1 = t[0].trim();
+                        t2 = t[1].trim();
+                        console.log("distance from origin: " + t1);
+                        console.log("distance from delivery: " + t2);
+
+                        let tt = $(tds[6]).text().trim();
+                        console.log("equipment: " + tt);
+
+
+                        tt = $(tds[8]).text().trim();
+                        console.log("distance: " + tt);
+
+                        t = $(tds[9]).text().trim().split("\n");
+                        t1 = t[0].trim();
+                        t2 = t[1].trim();
+                        console.log("weight: " + t1);
+                        console.log("weight type: " + t2);
+
+
+                        tt = $(tds[10]).text().trim();
+                        console.log("cmdty code: " + tt);
+
+                    });
+
+                }
+
+            }
+
+            await page.screenshot({path: '/home/ubuntu/screenshot/gaoshijie.png'});
+
+            console.log("当前url: " + page.url());
+        }
 
         await browser.close();
         console.log('done');

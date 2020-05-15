e52c20f59fa6c9317002b7f9afb51600752b24d8
qinglong.su <suqinglong01@gmail.com>
2020-05-15 18:02:54 +0800

    add JBHunt

--- src/sites/www.jbhunt.com.ts
+++ src/sites/www.jbhunt.com.ts
@@ -15,7 +15,7 @@ export class JBHunt extends SearchSite {
         await this.page.waitForSelector('.grouped-inputs:nth-child(1) p-autocomplete input[aria-autocomplete="list"]')
 
         // origin
-        this.log.log('input origin')
+        this.log.log('type origin')
         await this.page.type('.grouped-inputs:nth-child(1) p-autocomplete input[aria-autocomplete="list"]', task.criteria.origin, {
             delay: 10
         })
@@ -30,7 +30,7 @@ export class JBHunt extends SearchSite {
         await this.page.type('.grouped-inputs:nth-child(1) [formcontrolname="deadheadOrigin"]', task.criteria.origin_radius)
 
         // destination
-        this.log.log('input destination')
+        this.log.log('type destination')
         await this.page.type('.grouped-inputs:nth-child(2) p-autocomplete input[aria-autocomplete="list"]', task.criteria.destination, {
             delay: 10
         })
@@ -45,7 +45,7 @@ export class JBHunt extends SearchSite {
         await this.page.type('.grouped-inputs:nth-child(2) [formcontrolname="deadheadDestination"]', task.criteria.destination_radius)
 
         // equipment
-        this.log.log('input equipment')
+        this.log.log('type equipment')
         await this.page.evaluate(() => {
             (document.querySelector('.loadboard-input p-dropdown[formcontrolname="equipmentType"] .ui-dropdown') as HTMLElement).click()
         })
@@ -57,12 +57,15 @@ export class JBHunt extends SearchSite {
         }, task.criteria.equipment.toLowerCase())
         await this.page.click(`.loadboard-input .ui-dropdown-items p-dropdownitem:nth-child(${equipmentOptionIndex + 1})`);
 
+        this.log.log('click search btn')
         await this.page.click('.search-button');
         const searchUrl = this.page.url().replace(/StartDate=\d{4}-\d{2}-\d{2}/, 'StartDate=' + dateformat(task.criteria.pick_up_date, 'mm/dd/yyyy'))
         await this.page.goto(searchUrl, {
             waitUntil: 'domcontentloaded'
         })
         const response = await (await this.page.waitForResponse(this.searchAPI)).json() as any
+
+        this.log.log('has result')
 
         if (response?.data?.searchTripsElasticOptimized?.success) {
             const data = response.data.searchTripsElasticOptimized?.data?.loads
@@ -71,20 +74,24 @@ export class JBHunt extends SearchSite {
             ).then((res: any) => {
                 this.log.log(res.data);
             });
+        } else {
+            throw this.generateError('noData', 'no data')
         }
+
+        this.log.log('search done')
     }
 
     private getDataFromResponse(data:Array<any>):Array<IResultHTMLData> {
         const result:Array<IResultHTMLData> = data.map(item => {
             return {
+                ... item,
                 date: item.firstPickupDate,
                 equipment: item.equipment?.type,
                 origin: [item.firstPickupLocation?.location?.city , item.firstPickupLocation?.location?.state].join(', '),
                 origin_radius: item.deadheadOrigin + ' miles',
                 destination: [item.lastDeliveryLocation?.location?.city , item.lastDeliveryLocation?.location?.state].join(', '),
                 destination_radius: item.deadheadDestination + ' miles',
-                distance: item.totalMiles,
-                ... item
+                distance: item.totalMiles
             }
         })
         return result
